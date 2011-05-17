0153b038636ef5048f26d40d4b9f8f3136522f59
gw111zz <gw111zz@gmail.com>
2011-05-17 20:07:12 +0100

    Tidy up

--- src/com/helloscala/MyAndroidActivity.scala
+++ src/com/helloscala/MyAndroidActivity.scala
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 
 class MyAndroidActivity extends Activity {
-	
+
     override def onCreate(savedInstanceState:Bundle) : Unit = {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
--- src/com/helloscala/MyAndroidActivityJava.java
+++ src/com/helloscala/MyAndroidActivityJava.java
@@ -8,12 +8,7 @@ public class MyAndroidActivityJava extends MyAndroidActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		final TextView textView = (TextView) findViewById(R.id.mytext);
-		if (textView != null)
-            textView.setText("Hello");
 	}
-	
 }
 
 
