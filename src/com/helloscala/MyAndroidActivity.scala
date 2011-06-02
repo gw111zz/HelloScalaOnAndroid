d7127b821b617bb31f2bbfae0f11f9e835318625
Daniel Ribeiro <danrbr@gmail.com>
2011-06-02 17:38:47 -0300

    a more thorough example

--- src/com/helloscala/MyAndroidActivity.scala
+++ src/com/helloscala/MyAndroidActivity.scala
@@ -5,14 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import com.helloscala.R
 import util.Properties
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.TextView
 
 class MyAndroidActivity extends Activity {
 
     override def onCreate(savedInstanceState:Bundle) : Unit = {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        val myLayout = findViewById( R.id.mytext ).asInstanceOf[TextView]
+        myLayout.setText("This demo was done with: [" + Properties.versionMsg.toString()  + "]")
         Log.d("test", "yeah" + Properties.versionMsg.toString())
         Log.d("test", List(5, 6).map(_ * 2).toString())
+
     }
 }
-
