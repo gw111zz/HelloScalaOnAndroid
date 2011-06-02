fe65d59cb3f658aecf6403b08af6eb728cb36b96
Daniel Ribeiro <danrbr@gmail.com>
2011-06-02 16:51:47 -0300

    updating to the latest version of the android sdk

--- src/com/helloscala/MyAndroidActivity.scala
+++ src/com/helloscala/MyAndroidActivity.scala
@@ -2,12 +2,17 @@ package com.helloscala
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.helloscala.R
+import util.Properties
 
 class MyAndroidActivity extends Activity {
 
     override def onCreate(savedInstanceState:Bundle) : Unit = {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        Log.d("test", "yeah" + Properties.versionMsg.toString())
+        Log.d("test", List(5, 6).map(_ * 2).toString())
     }
 }
 
